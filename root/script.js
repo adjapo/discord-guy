8d9924d82c92a77382823ff4a1ffd717b0f1498d
adjapo <94869965+adjapo@users.noreply.github.com>
2025-04-27 17:45:11 -0700

    Create script.js

---
+++ root/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const header = document.querySelector("header h1");
+  header.addEventListener("click", function() {
+    alert("Welcome to My Website!");
+  });
+});
