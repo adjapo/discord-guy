d590665f1ed7b74d3f5fe42f60b5454dc96ec34e
adjapo <94869965+adjapo@users.noreply.github.com>
2025-04-27 17:52:04 -0700

    Delete root/script.js

--- root/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const header = document.querySelector("header h1");
-  header.addEventListener("click", function() {
-    alert("Welcome to My Website!");
-  });
-});
